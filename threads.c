781f9e8f7657c7325ffff16de4691b0839231a8c
James Wang <jnwang@suse.com>
2016-10-18 17:17:31 +0800

    it supports exit at once

--- threads.c
+++ threads.c
@@ -1,9 +1,9 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/kthread.h>  // for threads
-#include <linux/sched.h>  // for task_struct
-#include <linux/time.h>   // for using jiffies 
+#include <linux/time.h>
 #include <linux/timer.h>
+
 
 static struct task_struct *thread1;
 
@@ -12,29 +12,31 @@ int thread_fn(void *t) {
 
 unsigned long j0,j1;
 int delay = 60*HZ;
-
-printk(KERN_INFO "In thread1");
 j0 = jiffies;
 j1 = j0 + delay;
 
-while (time_before(jiffies, j1))
-        schedule();
-
+while (time_before(jiffies, j1)){ 
+if(kthread_should_stop()) {
+do_exit(0);
+}
+  schedule();
+}
+printk(KERN_INFO "In thread1");
 return 0;
 }
 
 int thread_init (void) {
-   
-    char  our_thread[8]="thread1";
-    printk(KERN_INFO "in init");
-    thread1 = kthread_create(thread_fn,NULL,our_thread);
-    if((thread1))
-        {
-        printk(KERN_INFO "in if");
-        wake_up_process(thread1);
-        }
+ 
+ char name[8]="thread1";
+ printk(KERN_INFO "in init");
+ thread1 = kthread_create(thread_fn,NULL,name);
+ if((thread1))
+  {
+  printk(KERN_INFO "in if");
+  wake_up_process(thread1);
+  }
 
-    return 0;
+ return 0;
 }
 
 
@@ -46,7 +48,7 @@ void thread_cleanup(void) {
   printk(KERN_INFO "Thread stopped");
 
 }
-MODULE_LICENSE("GPL");   
+MODULE_LICENSE("GPL"); 
 module_init(thread_init);
 module_exit(thread_cleanup);
 
